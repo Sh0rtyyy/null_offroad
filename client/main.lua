be980e4ec0edca1a53a94e9dc9754de90003da17
Shorty <shortyrealy@gmail.com>
2023-09-04 21:58:07 +0200

    Removed unused code

--- client/main.lua
+++ client/main.lua
@@ -9,49 +9,6 @@ RegisterNetEvent(clientEvent('toggleDebug'), function()
 	LocalPlayer.state:set('debuggingOffroad', not LocalPlayer.state['debuggingOffroad'])
 end)
 
-
---[[local Config.ClassMod = {
-    [0]= 1.31, -- Compacts 
-    [1] = 1.21, --Sedans
-    [2] = 1.01, --SUVs
-    [3] = 2.51, --Coupes
-    [4] = 2.201, --Muscle
-    [5] = 2.81, --Sports Classics
-    [6] = 2.51, --Sports
-    [7] = 3.51, --Super  
-    [8] = 1.51, --Motorcycles  
-    [9] = 0, --Off-road
-    [10] = 0, --Industrial
-    [11] = 0, --Utility
-    [12] = 5.21, --Vans  
-    [13] = 0, --Cycles  
-    [14] = 0, --Boats  
-    [15] = 0, --Helicopters  
-    [16] = 0, --Planes  
-    [17] = 0, --Service  
-    [18] = 0.21, --Emergency  
-    [19] = 0, --Military  
-    [20] = 0.21, --Commercial  
-    [21] = 0 --Trains  
-}]]
-
---[[local Config.WheelMod = {
-	[-1] = 1.91, --Stock
-    [0] = 1.31, --Sport 
-    [1] = 2.00, --Muscle
-    [2] = 1.81, --Lowrider
-    [3] = 0.50, --SUV
-    [4] = -1.001, --Offroad
-    [5] = 1.75, --Tuner
-    [6] = 1.61, --Bike Wheels
-    [7] = 1.91, --High Eend  
-    [8] = 1.21, --Bennys Original  
-    [9] = 0, --Bennys Bespoke 
-    [10] = 0, --Open Wheel
-    [11] = 0, --Street
-    [12] = 2.21, --Track  
-}]]
-
 local class = nil
 local wheelflag = nil
 
